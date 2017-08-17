80508ec04ecb68b6e2f436c3b8ba756bfd9f07c0
Mona Lisa <email@example.com>
2017-08-17 11:17:09 +0100

    Adding ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { Recipe } from './recipe.model' ;
+import { Ingredient } from '../shared/ingredient.model' ;
 
 @Injectable()
 export class RecipeService {
@@ -8,8 +9,21 @@ export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-    new Recipe('A Test Recipe 1', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Banjo_Shark_recipe.jpg/1280px-Banjo_Shark_recipe.jpg'),
-    new Recipe('A Test Recipe 2', 'This is a test', 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/Banjo_Shark_recipe.jpg/1280px-Banjo_Shark_recipe.jpg')
+    new Recipe('Toad in the hole',
+      'A lovely main meal - awesome',
+      'https://upload.wikimedia.org/wikipedia/commons/0/01/Toad_in_the_hole.jpg',
+    [
+      new Ingredient("Sausage", 10),
+      new Ingredient("Potato", 30)      
+    ]
+    ),
+    new Recipe('Carrot cake',
+      'A lovely sweet desert - great',
+      'https://upload.wikimedia.org/wikipedia/commons/8/87/Rueblitorte.jpg',
+      [
+        new Ingredient("Carrots", 50),
+        new Ingredient("Sugar", 300)      
+      ])
   ];
 
   constructor() { }
